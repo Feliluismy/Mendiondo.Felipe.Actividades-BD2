b4f705cc35a8edf5975e467714b98a380320c6bd
Feliluismy <felipemendiondodx@gmail.com>
2026-05-16 16:44:30 -0300

    actividad-7

--- semana-07/practica/respuestas.js
+++ semana-07/practica/respuestas.js
@@ -22,4 +22,15 @@ db.libros.find({$and: [{'autor.nacionalidad': {$eq: 'Española'}}, {'precio': {$
 "actividad-6"
 "Imprimir todas las series."
 db.series.find()
+db.series.find({titulo: {$eq : 'The big bang theory'}})
 db.series.find({titulo: 'The big bang theory'},{temporada1: 1})
+"Imprimir el nombre del primer capítulo de la primera temporada de la serie 'The Walking Dead'."
+db.series.find({titulo: 'The Walking Dead'},{_id:0, temporada1: {capitulo1:1}})
+
+"actividad-7"
+db.alumnos.find()
+"Imprimir el apellido y la fecha de nacimiento de todos los alumnos."
+db.alumnos.find({},{_id:0,apellido:1,fechanacimiento:1})
+"Imprimir el nombre y el apellido de los alumnos ordenados por fecha de nacimiento de forma descendente."
+db.alumnos.find().sort({fechanacimiento: -1})
+db.alumnos.find({fechanacimiento: {$gte: new Date('1970-01-01')}})
